00050531b22fad8b69b5e143016bfd5d7b081368
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-08-28 21:38:00 +0100

    fixed with proper atomic locks

--- src/ex1.8.1.zig
+++ src/ex1.8.1.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 const number_of_chopsticks = 5;
 const number_of_philosophers = 5;
+const number_of_eats = 10;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -9,28 +10,28 @@ pub fn main() !void {
 
     var chopsticks: [number_of_chopsticks]Chopstick = .{.{ .held = false }} ** number_of_chopsticks;
     var chopstick_context = ChopstickContext{
-        .locked = false,
+        .mutex = .{},
         .chopsticks = &chopsticks,
         .number_available = number_of_chopsticks,
         .philosopher_eat_count = [1]usize{0} ** number_of_philosophers,
         .philosopher_wait_count = [1]usize{0} ** number_of_philosophers,
     };
 
-    errdefer chopstick_context.print();
-
     var threads: [number_of_philosophers]std.Thread = undefined;
     for (0..number_of_philosophers) |i| {
         threads[i] = try std.Thread.spawn(
             .{ .allocator = allocator },
             act,
-            .{ .wait, &chopstick_context, i, 10 },
+            .{ .wait, &chopstick_context, i, number_of_eats },
         );
-        std.time.sleep(7 * std.time.ns_per_ms);
     }
 
     for (threads) |thread| {
         thread.join();
     }
+
+    std.debug.print("Finished!\n", .{});
+    chopstick_context.print();
 }
 
 const ActType = enum {
@@ -44,6 +45,7 @@ fn act(actType: ActType, chopstick_context: *ChopstickContext, name: usize, max_
     var run_count: usize = 0;
     while (run_count < max_runs) {
         const duration = 2 + rnd.random().int(usize) % 3;
+        const duration_ns = duration * std.time.ns_per_ms;
         switch (nextActType) {
             .wait => {
                 std.time.sleep(duration * std.time.ns_per_s);
@@ -52,9 +54,9 @@ fn act(actType: ActType, chopstick_context: *ChopstickContext, name: usize, max_
             .eat => {
                 const pickup_result = chopstick_context.pickupChopsticks(name);
                 if (pickup_result) |res| {
-                    std.time.sleep(duration * std.time.ns_per_s);
+                    std.time.sleep(duration * duration_ns);
                     while (!chopstick_context.putDownChopsticks(res, name)) {
-                        std.time.sleep(100 * std.time.ns_per_ms);
+                        std.time.sleep(duration_ns);
                     }
                     nextActType = .wait;
                     run_count += 1;
@@ -65,7 +67,7 @@ fn act(actType: ActType, chopstick_context: *ChopstickContext, name: usize, max_
 }
 
 const ChopstickContext = struct {
-    locked: bool,
+    mutex: std.Thread.Mutex,
     chopsticks: []Chopstick,
     number_available: usize,
     philosopher_wait_count: [number_of_philosophers]usize,
@@ -74,7 +76,6 @@ const ChopstickContext = struct {
     const Self = @This();
 
     fn print(self: Self) void {
-        std.debug.print("locked = {}\n", .{self.locked});
         std.debug.print("number_available = {}\n", .{self.number_available});
         std.debug.print("philosopher_wait_count, philosopher_eat_count\n", .{});
         for (self.philosopher_wait_count, self.philosopher_eat_count, 0..) |wait_count, eat_count, i| {
@@ -85,53 +86,46 @@ const ChopstickContext = struct {
     // return null if operation was blocked
     // else return indices of the two chopsticks picked up
     fn pickupChopsticks(self: *Self, name: usize) ?[2]usize {
-        // TODO - locked is a rubbish mutex, there's nothing to prevent you from checking it, then another thread altering it, then this thread altering it, need to replace with something atomic
-        if (self.locked) {
-            return null;
-        }
-
         if (self.number_available < 2) {
             return null;
         }
 
-        self.locked = true;
-        defer self.locked = false;
+        if (self.mutex.tryLock()) {
+            defer self.mutex.unlock();
 
-        var count: usize = 0;
-        var result: [2]usize = undefined;
-        for (self.chopsticks, 0..) |*chopstick, i| {
-            if (chopstick.held) {
-                continue;
-            }
-            chopstick.*.held = true;
-            result[count] = i;
-            count += 1;
-            self.*.number_available -= 1;
-            self.*.philosopher_eat_count[name] += 1;
-            if (count == 2) {
-                return result;
+            var count: usize = 0;
+            var result: [2]usize = undefined;
+            for (self.chopsticks, 0..) |*chopstick, i| {
+                if (chopstick.held) {
+                    continue;
+                }
+                chopstick.*.held = true;
+                result[count] = i;
+                count += 1;
+                self.*.number_available -= 1;
+                if (count == 2) {
+                    self.*.philosopher_eat_count[name] += 1;
+                    return result;
+                }
             }
         }
-
-        unreachable;
+        return null;
     }
 
     // return false if operation was blocked, else return true
     fn putDownChopsticks(self: *Self, indices: [2]usize, name: usize) bool {
-        // TODO - locked is a rubbish mutex, there's nothing to prevent you from checking it, then another thread altering it, then this thread altering it, need to replace with something atomic
-        if (self.locked) {
+        if (self.mutex.tryLock()) {
+            defer self.mutex.unlock();
+
+            for (indices) |idx| {
+                self.*.chopsticks[idx].held = false;
+                self.*.number_available += 1;
+            }
+            self.*.philosopher_wait_count[name] += 1;
+            return true;
+        } else {
             return false;
         }
-
-        self.locked = true;
-        defer self.locked = false;
-
-        for (indices) |idx| {
-            self.*.chopsticks[idx].held = false;
-            self.*.number_available += 1;
-        }
-        self.*.philosopher_wait_count[name] += 1;
-        return true;
     }
 };
 
